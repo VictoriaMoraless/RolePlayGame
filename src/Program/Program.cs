9eca79cbb23e232a2b093fea3040ca3b5d7e6f34
mcaetano7 <martina.caetano@correo.ucu.edu.uy>
2025-05-01 18:25:25 -0300

    modificaciones en program, creacion de enano!! modificacion de enano: ahora recibe un parametro solo porque la salud esta por default

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Library;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // creando 2 enanos
+        Dwarf dwarf1 = new Dwarf("Gimli");
+        
+        // agregar cosas
+        dwarf1.AddItem("Espada");
+        dwarf1.AddItem("Incienso");
+    }
+}
