1afb5e474fdd3211d8654096e1c36a74729ae380
VictoriaMoraless <victoria.moraless@correo.ucu.edu.uy>
2025-05-05 20:55:44 -0300

    Test RmItem

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -1,3 +1,5 @@
+using Library;
+
 namespace LibraryTests;
 
 public class Tests
@@ -8,8 +10,56 @@ public class Tests
     }
 
     [Test]
+   
+    //Ataque reduce salud correctamente
     public void Test1()
     {
+        
+        
+        
         Assert.Pass();
     }
+    
+    
+    
+    
+    
+    
+    //No se permite agregar el mismo item dos veces
+    public void NoDuplicidadItem()
+    {
+        
+        
+        Assert.Pass();
+    }
+    
+    
+    
+    
+    
+    //Se puede eliminar un ítem existente
+    public void TestRmItem()
+    {
+        Elf elf = new Elf("Angrod");
+
+        Items Gorro = new Items("Gorro", 0, 5);
+        elf.AddItem(Gorro);
+        elf.RmItem(Gorro);
+
+        Assert.IsFalse(elf.GetItems().Contains(Gorro));
+    }
+    
+    
+    
+    
+    
+    //Ataque no hace daño si ataque <= defensa
+    public void Test4()
+    {
+        
+        
+        
+        Assert.Pass();
+    }
+    
 }
