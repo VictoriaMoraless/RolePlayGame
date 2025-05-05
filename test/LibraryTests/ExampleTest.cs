8209be11fd41e67589f0f5f4ed1ad513f7f27d69
Mik4desuu <mikaela.maldonado@correo.ucu.edu.uy>
2025-05-05 19:51:23 -0300

    agrego los cuerpos de los test para no pisarnos

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -8,8 +8,54 @@ public class Tests
     }
 
     [Test]
+   
+    //Ataque reduce salud correctamente
     public void Test1()
     {
+        
+        
+        
         Assert.Pass();
     }
+    
+    
+    
+    
+    
+    
+    //No se permite agregar el mismo item dos veces
+    public void Test2()
+    {
+        
+        
+        
+        Assert.Pass();
+    }
+    
+    
+    
+    
+    
+    //Se puede eliminar un ítem existente
+    public void Test3()
+    {
+        
+        
+        
+        Assert.Pass();
+    }
+    
+    
+    
+    
+    
+    //Ataque no hace daño si ataque <= defensa
+    public void Test4()
+    {
+        
+        
+        
+        Assert.Pass();
+    }
+    
 }
