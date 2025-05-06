3b511dba6a18fe444d8e1b6d051ef146db6ac4fd
beluume <maria.mendes@correo.ucu.edu.uy>
2025-05-05 23:28:19 -0300

    Test ataque no hace daño si ataque <= defensa

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -58,7 +58,17 @@ public class Tests
         //Ataque no hace daño si ataque <= defensa
         public void Test4()
         {
+            Dwarf attacker = new Dwarf("Thorin");
+            Elf target = new Elf("Angrod");
 
-            Assert.Pass();
+            Items palito = new Items("Palito", 5, 20); // 5 de ataque, 20 de defensa
+            attacker.AddItem(palito); // daño total: 5
+            target.AddItem(palito);   // suma defensa: 20
+
+            int saludInicial = target.GetHealth();
+
+            attacker.Attack(target);
+
+            Assert.AreEqual(saludInicial, target.GetHealth(), "La salud no debería cambiar porque el ataque fue bloqueado.");
         }
 }
